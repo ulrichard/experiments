18f635c95c72d0d22ee86e1381fa075c05f7f4c2
Richard Ulrich <richi@paraeasy.ch>
2013-11-16 01:40:57 +0100

    completed my blog post at https://blog.ulrichard.ch/?p=1151#more-1151

--- enable_if/enable_if_test.cpp
+++ enable_if/enable_if_test.cpp
@@ -1,31 +1,73 @@
 #include <boost/range/concepts.hpp>
 #include <type_traits>
 #include <iostream>
+#include <sstream>
 #include <vector>
+#include <list>
+#include <iterator>
 
-template<class ContainerT, class StreamT/*, std::enable_if<boost::ForwardRangeConcept<ContainerT>*/>
+
+template <typename T>
+struct sfinae_true : std::true_type {};
+
+struct is_iterator_tester {
+    template <typename T>
+    static sfinae_true<typename std::iterator_traits<T>::iterator_category> test(int);
+
+    template <typename>
+    static std::false_type test(...);
+};
+
+template <typename T>
+struct is_iterator : decltype(is_iterator_tester::test<T>(0)) {};
+
+
+template<class ContainerT, class StreamT,
+        typename enabler = typename std::enable_if<is_iterator<decltype(std::begin(*static_cast<ContainerT*>(nullptr)))>::value, ContainerT>::type>
 StreamT& operator<<(StreamT& strm, const ContainerT& cont)
 {
 	strm << '{';
 	for(const auto& element : cont)
-		strm << element;
+		strm << element << ' ';
 	strm << '}';
+
+	return strm;
 }
 
-struct SomeType
+struct Base
 {
-	int one;
-	int another;
+    virtual std::string Describe() const = 0;
+};
+
+std::ostream& operator<<(std::ostream& strm, const Base& base)
+{
+    strm << base.Describe() << " ";
+}
+
+struct SomeType : public Base
+{
+    SomeType(int p1, int p2) : one_(p1), another_(p2) { }
+
+    virtual std::string Describe() const
+    {
+        std::stringstream sstr;
+        sstr << '{' << one_ << ' ' << another_ << '}';
+        return sstr.str();
+    }
+
+	int one_;
+	int another_;
 };
 
 int main()
 {
-	std::vector<int> numbers{8, 45, 87, 90, 99999};
-	std::cout << numbers;
+	std::vector<int> ints{8, 45, 87, 90, 99999};
+	std::list<float> floats{3.14159, 2.71828, 0.57721, 1.618033};
+	std::cout << ints << floats;
 
 	SomeType some{456, 789};
 	std::cout << some;
-	
+
 	return 0;
 }
 
