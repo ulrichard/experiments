eb20781726f3d553448755e84a55db38b8e0188a
Richard Ulrich <richi@paraeasy.ch>
2014-10-27 17:58:42 +0100

    url

--- initializerlists/sutter1.cpp
+++ initializerlists/sutter1.cpp
@@ -5,14 +5,15 @@ int a = 42;        // still fine, as always
 auto x = begin(v); // no narrowing or non-initialization is possible
 
 // C++98
-rectangle       w( origin(), extents() );   // oops, declares a function, if origin and extents are types
-complex<double> c( 2.71828, 3.14159 );
-int             a[] = { 1, 2, 3, 4 };
+rectangle       w(origin(), extents());   // oops, declares a function, if origin and extents are types
+complex<double> c(2.71828, 3.14159);
+int             a[] = {1, 2, 3, 4};
 vector<int>     v;
-for( int i = 1; i <= 4; ++i ) v.push_back(i);
+for(int i = 1; i <= 4; ++i) v.push_back(i);
 
 // C++11
-rectangle       w   { origin(), extents() };
-complex<double> c   { 2.71828, 3.14159 };
-int             a[] { 1, 2, 3, 4 };
-vector<int>     v   { 1, 2, 3, 4 };
+rectangle       w   {origin(), extents()};
+complex<double> c   {2.71828, 3.14159};
+int             a[] {1, 2, 3, 4};
+vector<int>     v   {1, 2, 3, 4};
+
--- initializerlists/sutter2.cpp
+++ initializerlists/sutter2.cpp
@@ -1,15 +1,15 @@
 // C++98
-X::X( /*...*/ ) : mem1(init1), mem2(init2, init3) { /*...*/ }
+X::X(/*...*/) : mem1(init1), mem2(init2, init3) {/*...*/}
 
 // C++11
-X::X( /*...*/ ) : mem1{init1}, mem2{init2, init3} { /*...*/ }
+X::X(/*...*/) : mem1{init1}, mem2{init2, init3} {/*...*/}
 
 
 
-void draw_rect( rectangle );
+void draw_rect(rectangle);
  
 // C++98
-draw_rect( rectangle( myobj.origin, selection.extents ) );
+draw_rect(rectangle(myobj.origin, selection.extents));
 
 // C++11
-draw_rect( { myobj.origin, selection.extents } );
+draw_rect({myobj.origin, selection.extents});
